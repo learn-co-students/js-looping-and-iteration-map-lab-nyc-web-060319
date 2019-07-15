5fa9bdaa3f64c49c1e5eec5035ecf33575de34e9
Alexander Riccio <test35965+learn@gmail.com>
2019-07-15 09:56:10 -0400

    solve lab so easy for a monday morn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+'use strict'
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+    return drivers.map(function(driver) {return driver.toLowerCase();});
+}
+
+function nameToAttributes(drivers) {
+    return drivers.map(function(driver) {
+        
+        const attrib = driver.split(" ");
+        const newDriver = {firstName: attrib[0], lastName: attrib[1]};
+        return newDriver;
+    });
+}
+
+function attributesToPhrase(drivers) {
+    return drivers.map(function(driver) {return `${driver.name} is from ${driver.hometown}`});
+}

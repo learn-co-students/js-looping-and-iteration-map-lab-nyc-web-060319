11b6d09163ee17a9b2800969c1b0032abf66511f
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-16 10:10:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+    return drivers.map(function(driver) { return driver.toLowerCase(); }); 
+}
+
+function nameToAttributes(drivers){
+    let returnObj = {};
+     return drivers.map(function(driver){
+        return {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]}
+    })
+}
+
+function attributesToPhrase(phrases){
+    let returnObj = {};
+     return phrases.map(function(phrase){
+        return  phrase.name +  " is from " + phrase.hometown;
+    });
+}
+
